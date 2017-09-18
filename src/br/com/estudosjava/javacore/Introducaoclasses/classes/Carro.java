1c86ea2730834760f2412e47b8822d81c45783c9
Kaio Ferreira <kaioferreiracosta@hotmail.com>
2017-09-18 12:14:01 -0300

    Initial commit

---
+++ src/br/com/estudosjava/javacore/Introducaoclasses/classes/Carro.java
@@ -0,0 +1,10 @@
+package br.com.estudosjava.javacore.Introducaoclasses.classes;
+
+public class Carro {
+
+    public String placa;
+    public String modelo;
+    public float velocidadeMaxima;
+
+
+}
