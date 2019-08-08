53549b89549d73f2ff9174aace7fc83cf2efacc3
kaioferreiira <kaioferreiracosta@hotmail.com>
2019-08-08 09:16:56 -0300

    Criando pastas java  13

---
+++ src/br/com/estudosjava/Java13/Java13.java
@@ -0,0 +1,21 @@
+package br.com.estudosjava.Java13;
+
+public class Java13 {
+
+    public static void main(String[] args) {
+
+//        System.out.println("""
+//                Hello,
+//                multiline
+//                text blocks!""");
+// prints:
+// > Hello,
+// > multiline
+// > text blocks!
+// wait, where did the indentation go?
+
+
+
+
+    }
+}
