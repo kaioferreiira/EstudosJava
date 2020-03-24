5abb2fee74d857019e44694787ad5d0e4d827123
Kaio Ferreira <63658991-Oiak>
2020-03-24 10:44:35 -0300

    SwitchExpressions

--- src/br/com/estudosjava/Java14/SwitchExpressions.java
+++ src/br/com/estudosjava/Java14/SwitchExpressions.java
@@ -1,4 +1,23 @@
 package br.com.estudosjava.Java14;
 
 public class SwitchExpressions {
+
+	public static void main(String[] args) {
+
+
+		String day = "TH";
+		String result = switch (day) {
+			case "M", "W", "F" -> "MWF";
+			case "T", "TH", "S" -> "TTS";
+			default -> {
+				if(day.isEmpty())
+					yield "Please insert a valid day.";
+                else
+				yield "Looks like a Sunday.";
+			}
+
+		};
+		System.out.println(result);
+
+	}
 }
